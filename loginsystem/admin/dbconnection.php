f1512bbdcb38f0f8fe827a07e49970d2851ac97a
Unknown <hkp.anup@gmail.com>
2018-07-10 19:57:44 +0530

    Change in db config

--- loginsystem/admin/dbconnection.php
+++ loginsystem/admin/dbconnection.php
@@ -1,8 +1,8 @@
 <?php
 define('DB_SERVER','localhost');
 define('DB_USER','root');
-define('DB_PASS' ,'root');
-define('DB_NAME', 'loginsystem');
+define('DB_PASS' ,'Kiran1993');
+define('DB_NAME', 'bidaai');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 
 // Check connection
